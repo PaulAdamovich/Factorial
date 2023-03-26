df3c3edb08fbacb8fd2803a9fead67cbba7e165c
PaulAdamovich <117354046+PaulAdamovich@users.noreply.github.com>
2023-03-26 20:24:32 +0400

    three

--- src/Factorial.java
+++ src/Factorial.java
@@ -2,7 +2,7 @@ public class Factorial {
     private static Long calculate(long i){
         long result = 1;
         if(i < 0){
-            System.out.println("Why do you need factorial from the negative number?");
+            System.out.println("Why did you need factorial from the negative number?");
             return null;
         }
         if (i == 0){
