8a1d2a3ddf3c6a4b27c5c3eb16969591518f1485
PaulAdamovich <117354046+PaulAdamovich@users.noreply.github.com>
2023-03-26 20:23:25 +0400

    second

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,9 +1,18 @@
 public class Factorial {
-    private void calculate(int i){
-
-
+    private static Long calculate(long i){
+        long result = 1;
+        if(i < 0){
+            System.out.println("Why do you need factorial from the negative number?");
+            return null;
+        }
+        if (i == 0){
+            return result;
+        }
+        result = i * calculate(i-1);//Recursion
+        return result;
     }
 
     public static void main(String[] args) {
+        System.out.println("Factorial = " + Factorial.calculate(13));
     }
 }
